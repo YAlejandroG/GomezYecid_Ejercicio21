26308e2a66695a2ecee6228d1499fe4144abe607
Default user <yalejandro.gomez@gmail.com>
2019-10-22 18:46:03 +0000

    Grafica corregida

--- Ejercicio21.py
+++ Ejercicio21.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pylab as plt
 
 x = np.linspace(0,np.pi*2,100)
-y = np.sin(x)
+y = np.cos(x)
 plt.plot(x,y,c='g')
 plt.grid()
 plt.show()
+plt.savefig('Cos(x).png')
